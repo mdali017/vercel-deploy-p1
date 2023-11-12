225e11159636a56e5d647efa9a3b6e9dba526cd1
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-11-12 18:56:05 +0600

    message

--- src/app.js
+++ src/app.js
@@ -5,12 +5,18 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.status(200).json("Welcome to vercel server.");
+  res.status(200).json({
+    success: true,
+    message: "Welcome to vercel server.",
+  });
 });
 
 // all product
 app.get("/products", (req, res) => {
-  res.send("All Product is here");
+  res.status(200).json({
+    success: true,
+    message: "All Product is here",
+  });
 });
 
 module.exports = app;
