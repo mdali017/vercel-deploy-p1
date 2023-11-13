8cbc7dc2650a1896fe9ec1d10eabff04247b0cf5
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-11-13 18:08:06 +0600

    mongodb connection with drive

--- src/app.js
+++ src/app.js
@@ -11,6 +11,55 @@ app.use(express.urlencoded({ extended: true }));
 // // user router
 // app.use("/api/users", userRouter);
 
+// ==================== mongodb connecteion start
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER1}:${process.env.DB_PASS1}@cluster0.5julrfk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const reviewCollection = client.db("BistroDBP1").collection("reviews");
+
+    // For All users
+    app.get("/reviews", async (req, res) => {
+      try {
+        const result = await reviewCollection.find().toArray();
+        res.status(200).json({
+          success: true,
+          message: "All users is here..",
+          users: result,
+        });
+      } catch (error) {
+        console.log(error.message);
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// ==================== mongodb connecteion end
+
 // For All users
 app.get("/api/users", async (req, res) => {
   try {
